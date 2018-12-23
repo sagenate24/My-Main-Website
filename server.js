2e82077f356af05b78a68d1bb7b3f4b702f2bc2f
Nathan Sage <nathansage@Nathans-MacBook-Pro.local>
2018-12-22 19:24:35 -0800

    created email messaging functionality

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const secret = require('./secret')
 const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -7,6 +11,50 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/build/index.html'))
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/form', (req, res) => {
+  nodemailer.createTestAccount((err, account) => {
+    const htmlEmail = `
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Name: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: secret.user,
+        pass: secret.pass
+      }
+    })
+
+    let mailOptions = {
+      from: req.body.email,
+      to: 'sagenate24@gmail.com',
+      replyTo: req.body.email,
+      subject: req.body.name,
+      text: req.body.message,
+      html: htmlEmail
+    }
+
+    console.log(req.body)
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        return console.log(err)
+      }
+
+      console.log('Message sent: %s', info.messageId)
+    })
+  })
+})
 
 const port = process.env.PORT || 3000;
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,11 +33,11 @@ class Contact extends Component {
       message
     })
 
-    this.setState({
-      name: '',
-      email: '',
-      message: '',
-    })
+    // this.setState({
+    //   name: '',
+    //   email: '',
+    //   message: '',
+    // })
   }
 
   checkEmail = (e) => {
@@ -54,46 +54,45 @@ class Contact extends Component {
     const { name, email, emailInvalid, message } = this.state;
 
     return (
-        <div className='contact'>
-          <SectionTitle padding='60px 0 20px' color='#f8f8f8'>CONTACT</SectionTitle>
-          <h2>Leave your name, email and a message and i will get back to you as soon as I can 😄.</h2>
-          <form className='contact_form' onSubmit={this.handleSubmit}>
-            <input
-              value={name}
-              type='text'
-              name='name'
-              placeholder='Name'
-              className='contact_input_fields'
-              onChange={this.handleChange}
-            />
-            <input
-              value={email}
-              type='email'
-              ref={this.emailInput}
-              onBlur={this.checkEmail}
-              name='email'
-              placeholder='Email'
-              className='contact_input_fields'
-              onChange={this.handleChange}
-            />
-            {emailInvalid && (
-              <label className='error_label'>The email address you supplied is invalid.</label>
-            )}
-
-            <textarea
-              value={message}
-              name='message'
-              placeholder='Message'
-              className='contact_textarea'
-              onChange={this.handleChange}
-            />
-            <button
-              type='submit'
-              className='send_button'
-              disabled={name === '' || email === '' || message === ''}
-            >SEND</button>
-          </form>
-        </div>
+      <div className='contact'>
+        <SectionTitle padding='60px 0 20px' color='#f8f8f8'>CONTACT</SectionTitle>
+        <h2>Leave your name, email and a message and i will get back to you as soon as I can 😄.</h2>
+        <form className='contact_form' onSubmit={this.handleSubmit}>
+          <input
+            value={name}
+            type='text'
+            name='name'
+            placeholder='Name'
+            className='contact_input_fields'
+            onChange={this.handleChange}
+          />
+          <input
+            value={email}
+            type='email'
+            name='email'
+            ref={this.emailInput}
+            onBlur={this.checkEmail}
+            placeholder='Email'
+            className='contact_input_fields'
+            onChange={this.handleChange}
+          />
+          {emailInvalid && (
+            <label className='error_label'>The email address you supplied is invalid.</label>
+          )}
+          <textarea
+            value={message}
+            name='message'
+            placeholder='Message'
+            className='contact_textarea'
+            onChange={this.handleChange}
+          />
+          <button
+            type='submit'
+            className='send_button'
+            disabled={name === '' || email === '' || message === ''}
+          >SEND</button>
+        </form>
+      </div>
     );
   }
 }
